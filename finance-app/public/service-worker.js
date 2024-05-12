7890ca13d831c0254e2c3cc7aa31744752bfa427
wkrolik-th <wkrolik@smarttech247.com>
2024-05-12 13:03:20 +0200

    craco build

--- finance-app/public/service-worker.js
+++ finance-app/public/service-worker.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-restricted-globals */
 self.addEventListener('push', event => {
-    const data = event.data.json();
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: '/logo192.png'
-    });
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: '/logo192.png'
   });
-  
+});
--- finance-app/src/service-worker.js
+++ finance-app/src/service-worker.js
@@ -1,10 +1,10 @@
-self.addEventListener('push', function(event) {
-    const payload = event.data ? event.data.text() : 'No payload';
-    event.waitUntil(
-      self.registration.showNotification('Financial Alert!', {
-        body: payload,
-        icon: 'icons/icon-192x192.png'
-      })
-    );
-  });
-  
+/* eslint-disable no-restricted-globals */
+self.addEventListener('push', function (event) {
+  const payload = event.data ? event.data.text() : 'No payload';
+  event.waitUntil(
+    self.registration.showNotification('Financial Alert!', {
+      body: payload,
+      icon: 'icons/icon-192x192.png'
+    })
+  );
+});
