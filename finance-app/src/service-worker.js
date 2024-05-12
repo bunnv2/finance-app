39a4cdd3db02f213716a6d59795363a118cc879c
bunnv2 <bylic22@gmail.com>
2024-05-12 18:23:55 +0200

    workbox fix

--- finance-app/src/service-worker.js
+++ finance-app/src/service-worker.js
@@ -1,14 +1,20 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
-
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
 
 self.addEventListener('push', function (event) {
   const payload = event.data ? event.data.text() : 'No payload';
   event.waitUntil(
     self.registration.showNotification('Financial Alert!', {
       body: payload,
-      icon: 'icons/icon-192x192.png'
+      icon: '../public/bunn.png'
     })
   );
 });
+
+
