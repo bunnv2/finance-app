db158f6dbb6a96190f7fd351f25e61349a699b59
bunnv2 <bylic22@gmail.com>
2024-05-12 17:43:14 +0200

    eslint config

--- finance-app/src/service-worker.js
+++ finance-app/src/service-worker.js
@@ -1,4 +1,8 @@
-/* eslint-disable no-restricted-globals */
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+// Workbox with custom configuration
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
 self.addEventListener('push', function (event) {
   const payload = event.data ? event.data.text() : 'No payload';
   event.waitUntil(
